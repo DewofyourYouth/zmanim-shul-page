b3a5aee95372fafbbe56e95671b871384a9da532
Yaakov <jacobshore@gmail.com>
2018-04-02 10:58:33 +0300

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function setup(){
+				loadJSON("http://www.hebcal.com/shabbat/?cfg=json&geo=geoname&amp;geonameid=295277&amp;D=on&amp;lg=h&amp;m=50&amp;o=on&amp;cfg=j&amp;tgt=_top", gotData);
+
+}
+
+function gotData(data){
+	let today = data.items[0].title;
+	let parsha = data.items[7].title;
+	let candlelighting = data.items[6].title;
+	let havdala = data.items[8].title;
+	document.getElementById('today').innerHTML = today
+	document.getElementById('parsha').innerHTML = "פרשת השבוע: " + parsha;
+	document.getElementById('candlelighting').innerHTML = candlelighting;
+	document.getElementById('havdala').innerHTML =  havdala;
+}
