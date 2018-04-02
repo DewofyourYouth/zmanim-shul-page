741753575a877f9550ecc153cb951ee20588219f
Yaakov <jacobshore@gmail.com>
2018-04-02 13:56:06 +0300

    date changes

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ function gotData(data){
 	let parsha = data.items[7].title;
 	let candlelighting = data.items[6].title;
 	let havdala = data.items[8].title;
-	let todayDate = new Date();
-	let daysOfWeek = ["יום ראשון", "יום שני", "יום שלישי", "יום רביעי", "יום חמישי", "יום ששי", "שבת קודש"];
-	let todayWeek = daysOfWeek[todayDate.getDay()];
-	document.getElementById('today').innerHTML = todayWeek + ", " + today;
+
+	document.getElementById('today').innerHTML = today;
 	document.getElementById('parsha').innerHTML = "פרשת השבוע: " + parsha;
 	document.getElementById('candlelighting').innerHTML = candlelighting;
 	document.getElementById('havdala').innerHTML =  havdala;
