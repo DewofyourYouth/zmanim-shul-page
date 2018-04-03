bd3bdaf27ff7e3d0995934e462860032d320ae74
Yaakov <jacobshore@gmail.com>
2018-04-03 20:35:58 +0300

    added omer

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const shabbatTimes = new XMLHttpRequest();
 const d = new Date();
+const dayOfWeek = d.getDay();
 const date = d.toISOString();
+let daysOfWeek = ["יום ראשון", "יום שני", "יום שלישי", "יום רביעי", "יום חמישי", "יום ששי", "שבת קודש"];
 const today = date.substr(0, 10);
 
 console.log(today);
@@ -18,3 +20,23 @@ shabbatTimes.onload = function() {
 	}
 };
 shabbatTimes.send();
+
+const jewishCal = new XMLHttpRequest();
+jewishCal.open('GET', 'http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&o=on&min=on&d=onmod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=295432&m=50&s=on');
+jewishCal.onload = function() {
+	const jewCal = JSON.parse(jewishCal.responseText);
+	let i = 0;
+	while(jewCal.items[i].date !== today){
+		i++;
+	} 
+	console.log(i)
+	function omerDay(element){
+		return element.category === 'omer' && element.date === today;
+	}
+	omer = jewCal.items.findIndex(omerDay);
+	omerString = jewCal.items[omer].hebrew;
+	console.log(omerString);
+	console.log(jewCal.items[i]);
+	document.getElementById('date').innerHTML = daysOfWeek[dayOfWeek] + ", " + jewCal.items[i].hebrew + "<br>"  +  "<span style='color:555; font-size: 1.3rem;'>" + omerString + "</span>";
+}
+jewishCal.send();
